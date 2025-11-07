8975f3321c88f927b485bdec6e5908fd66dbc732
Sunha-i <icecream20@khu.ac.kr>
2025-11-08 03:53:34 +0900

    chore: add FBX SDK DLL copy to post-build and unify library path naming
    - debug to Debug (to use the automated variable $(Configuration))
    - libfbxsdk.dll needs to be included in lib folder

---
+++ Mundi/Source/Editor/FBXImporter.cpp
@@ -0,0 +1,15 @@
+﻿#include "pch.h"
+#include "FBXImporter.h"
+
+bool FFBXImporter::ImportFBX(const FString& FilePath, USkeletalMesh* OutMesh, FFBXSkeletonData* OutSkeleton, const FFBXImportOptions& Options)
+{
+	return false;
+}
+
+void FFBXImporter::ParseMesh(FbxMesh* Mesh, FFBXMeshData& OutMeshData)
+{
+}
+
+void FFBXImporter::ParseSkeleton(FbxNode* Root, FFBXSkeletonData& OutSkeleton)
+{
+}
---
+++ Mundi/Source/Editor/FBXImporter.h
@@ -0,0 +1,52 @@
+﻿#pragma once
+
+#include "FBXSDK.h"
+#include "UEContainer.h"
+#include "Vector.h"
+
+class USkeletalMesh;
+
+struct FFBXImportOptions
+{
+	bool bImportAnimation = false;
+	bool bImportNormals = false;
+	bool bConvertToRightHanded = true;
+};
+
+struct FFBXMeshData
+{
+	TArray<FVector> Positions;
+	TArray<FVector> Normals;
+	TArray<FVector2D> TexCoords;
+
+	TArray<int> BoneIndices;
+	TArray<float> BoneWeights;
+	TArray<uint32> Indices;
+};
+
+struct FFBXSkeletonData
+{
+	struct FBone
+	{
+		FString Name;
+		int32 ParentIndex;
+		FMatrix BindPose;
+		FMatrix InverseBindPose;
+	};
+	TArray<FBone> Bones;
+};
+
+class FFBXImporter
+{
+public:
+	static bool ImportFBX(
+		const FString& FilePath, 
+		USkeletalMesh* OutMesh,
+		FFBXSkeletonData* OutSkeleton, 
+		const FFBXImportOptions& Options = FFBXImportOptions()
+	);
+
+private:
+	static void ParseMesh(FbxMesh* Mesh, FFBXMeshData& OutMeshData);
+	static void ParseSkeleton(FbxNode* Root, FFBXSkeletonData& OutSkeleton);
+};
